33332f2bf38188dfcbcb7a9e6c36cf346f40d199
Manishngk99 <Manishngk99>
2023-07-17 20:16:37 +0545

    EStudent and IStudent added

---
+++ Library.Domain/Entities/EStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class EStudent
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email{ get; set; }
+        public string Faculty{ get; set; }
+        public int RollNo { get; set; }
+        public int StudentCode{ get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+
+    }
+}

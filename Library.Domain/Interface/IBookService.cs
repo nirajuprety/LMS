d4b8a683c587882ae53c5139e0cad5ecbf7ab9db
kalyansharma-03 <sharmakalyan25@gmail.com>
2023-07-18 11:01:47 +0545

    Migration removed

---
+++ Library.Domain/Interface/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using Library.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Interface
+{
+    public interface IBookService
+    {
+        Task<bool> AddBook(EBook eBook);
+        Task<List<EBook>> GetBooks();
+        Task<EBook> GetBookByBookID(int id);
+        Task<bool> UpdateBookStatus(EBook eBook);
+        Task<bool> DeleteBook(int id);
+    }
+}

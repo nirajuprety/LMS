c0acb93eb826bfbf8a23f87f4ff89acc8e2a9f83
nirajuprety <nerajupreti@gmail.com>
2023-07-18 13:28:28 +0545

    Scoped Added and Common class created

---
+++ Library.Infrastructure/Service/Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Service
+{
+    public class Common
+    {
+
+        public class ServiceResult<t>
+        {
+            public StatusType Status { get; set; }
+            public string Message { get; set; }
+            public t Data { get; set; }
+        }
+    }
+    public enum StatusType
+    {
+        Success,
+        Failure,
+    }
+}
